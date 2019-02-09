0b138b6c9f4080bf5e530d6578898e39c74fe510
Doug Sheppard <doug@sirilyan.com>
2019-02-09 14:00:16 -0500

    get rid of remoteMac by adding isRemote()

--- commands.js
+++ commands.js
@@ -1,48 +1,54 @@
 const exec = require('child_process').exec
 const ssh2 = require('ssh2').Client
 const fs = require('fs')
+const os = require('os')
+
+const isRemote = (hostname, user, keyFile) => {
+    return (user && keyFile && hostname != os.hostname())
+}
 
 module.exports = {
     mac: {
-        turnOn: function() {
-            exec("caffeinate -u -t 5")
-        },
-        turnOff: function() {
-            exec("pmset displaysleepnow")
-        }
-    },
-    remoteMac: {
         turnOn: (hostname, user, keyFile) => {
-            conn = new ssh2()
-            conn.on('ready', () => {
-                conn.exec("caffeinate -u -t 5", (err, stream) => {
-                    stream.on('close', () => { conn.end() })
+            const command = "caffeinate -u -t 5"
+
+            if (isRemote()) {
+                conn = new ssh2()
+                conn.on('ready', () => {
+                    conn.exec(command, (err, stream) => {
+                        stream.on('close', () => { conn.end() })
+                    })
+                }).connect({
+                    host: hostname,
+                    username: user,
+                    privateKey: fs.readFileSync(keyFile)
                 })
-            }).connect({
-                host: hostname,
-                username: user,
-                privateKey: fs.readFileSync(keyFile)
-            })
+            } else {
+                exec(command);
+            }
         },
         turnOff: (hostname, user, keyFile) => {
-            console.log(user + "@" + hostname + ": key " + keyFile)
-            conn = new ssh2()
-            conn.on('ready', () => {
-                conn.exec("pmset displaysleepnow", (err, stream) => {
-                    stream.on('close', () => { conn.end() })
+            const command = "pmset displaysleepnow"
+
+            if (isRemote()) {
+                conn = new ssh2()
+                conn.on('ready', () => {
+                    conn.exec(command, (err, stream) => {
+                        stream.on('close', () => { conn.end() })
+                    })
+                }).connect({
+                    host: hostname,
+                    username: user,
+                    privateKey: fs.readFileSync(keyFile)
                 })
-            }).connect({
-                host: hostname,
-                username: user,
-                privateKey: fs.readFileSync(keyFile)
-            })
+            } else {
+                exec(command);
+            }
         }
     },
     windows: {
-        turnOn: function() {
-
-        },
-        turnOff: function() {
+        turnOn: (hostname, user, keyFile) => {},
+        turnOff: (hostname, user, keyFile) => {
             exec("powershell (Add-Type '[DllImport(\"user32.dll\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2")
         }
     }
