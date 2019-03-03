d30f1795c511e349fc94cde216b14b84ae692263
sirilyan <doug@sirilyan.com>
2019-03-03 01:59:36 -0500

    Satisfy ESlint
    Conform to Airbnb style guide
    Convert prototype-based definition to class

--- commands.js
+++ commands.js
@@ -1,63 +1,65 @@
-"use strict"
+const os = require('os');
+const NodeSsh = require('node-ssh');
+const cpp = require('child-process-promise');
 
-const exec = require('child-process-promise').exec
-const node_ssh = require('node-ssh')
-const os = require('os')
+const { localhost, exec, isAwake } = {
+  localhost: os.hostname,
+  exec: cpp.exec,
+  isAwake: [],
+};
 
-const _isSleeping = []
+const isRemote = hostname => hostname !== localhost();
 
-const isRemote = (hostname) => {
-    return (hostname && hostname != os.hostname())
-}
+const remoteExec = (hostname, user, keyFile, command) => {
+  const conn = new NodeSsh();
+  return conn.connect({
+    host: hostname,
+    username: user,
+    privateKey: keyFile,
+  }).then(() => conn.execCommand(command));
+};
+
+const localExec = command => exec(command);
 
 const runCommand = async (hostname, user, keyFile, command) => {
-    let r
-    if (isRemote(hostname)) {
-        r = _remoteExec(hostname, user, keyFile, command)
-    } else {
-        r = _localExec(command)
-    }
-    return r
-}
-
-const _remoteExec = async (hostname, user, keyFile, command) => {
-    let conn = new node_ssh()
-    return conn.connect({
-        host: hostname,
-        username: user,
-        privateKey: keyFile
-    }).then(() => conn.execCommand(command))
-}
-
-const _localExec = (command) => {
-    return exec(command)
-}
+  let r;
+  if (isRemote(hostname)) {
+    r = remoteExec(hostname, user, keyFile, command);
+  } else {
+    r = localExec(command);
+  }
+  return r;
+};
 
 module.exports = {
-    mac: {
-        isOn: (hostname) => !_isSleeping[hostname],
-        updateOn: async (hostname, user, keyFile) => {
-            const command = "pmset -g powerstate IODisplayWrangler"
-            let result = await runCommand(hostname, user, keyFile, command)
-            _isSleeping[hostname] = result.stdout.indexOf("USEABLE") == -1
-            console.log(_isSleeping)
-        },
-        turnOn: (hostname, user, keyFile) => {
-            const command = "caffeinate -u -t 5"
-            runCommand(hostname, user, keyFile, command)
-            .then(() => _isSleeping[hostname] = false)
-        },
-        turnOff: (hostname, user, keyFile) => {
-            const command = "pmset displaysleepnow"
-            runCommand(hostname, user, keyFile, command)
-            .then(() => _isSleeping[hostname] = true)
-        }
+  mac: {
+    isOn: hostname => isAwake[hostname],
+    updateOn: async (hostname, user, keyFile) => {
+      const command = 'pmset -g powerstate IODisplayWrangler';
+      isAwake[hostname] = await runCommand(hostname, user, keyFile, command)
+        .then(result => result.stdout.includes('USEABLE'));
     },
-    windows: {
-        turnOn: (hostname, user, keyFile) => { },
-        turnOff: (hostname, user, keyFile) => {
-            exec("powershell (Add-Type '[DllImport(\"user32.dll\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2")
-        }
-    }
-}
-
+    turnOn: async (hostname, user, keyFile) => {
+      const command = 'caffeinate -u -t 5';
+      isAwake[hostname] = await runCommand(hostname, user, keyFile, command)
+        .then(() => true);
+    },
+    turnOff: async (hostname, user, keyFile) => {
+      const command = 'pmset displaysleepnow';
+      isAwake[hostname] = await runCommand(hostname, user, keyFile, command)
+        .then(() => false);
+    },
+  },
+  windows: {
+    turnOn: async (hostname, user, keyFile) => {
+      const command = 'echo hi';
+      isAwake[hostname] = await runCommand(hostname, user, keyFile, command)
+        .then(() => false);
+    },
+    turnOff: async (hostname, user, keyFile) => {
+      const command = "powershell (Add-Type '[DllImport(\"user32.dll\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2";
+      isAwake[hostname] = await runCommand(hostname, user, keyFile, command)
+        .then(() => true);
+    },
+  },
+};
--- index.js
+++ index.js
@@ -1,72 +1,70 @@
-"use strict"
+const OsCommands = require('./commands');
 
-const OsCommands = require('./commands')
+let Service;
+let Characteristic;
 
-let Service, Characteristic
-
-module.exports = function (homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-screen-standby", "ComputerScreen", ComputerScreen);
-}
-
-function ComputerScreen(log, config, api) {
+class ComputerScreen {
+  constructor(log, config) {
     this.log = log;
-    this.name = config["name"]
-    this.osType = config["osType"]
-    this.hostname = config["hostname"]
-    this.username = config["username"]
-    this.sshKey = config["sshKey"]
+    this.name = config.name;
+    this.osType = config.osType;
+    this.hostname = config.hostname;
+    this.username = config.username;
+    this.sshKey = config.sshKey;
 
     this.screenService = new Service.Switch(this.name);
 
     this.screenService
-        .getCharacteristic(Characteristic.On)
-        .on('get', this.getSwitchOnCharacteristic.bind(this))
-        .on('set', this.setSwitchOnCharacteristic.bind(this));
-}
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getSwitchOnCharacteristic.bind(this))
+      .on('set', this.setSwitchOnCharacteristic.bind(this));
+  }
 
-ComputerScreen.prototype.getServices = function getServices() {
-    return [this.screenService]
-}
+  getServices() {
+    return [this.screenService];
+  }
 
-ComputerScreen.prototype.getSwitchOnCharacteristic = async function (next) {
+  async getSwitchOnCharacteristic(next) {
     let commands;
     switch (this.osType) {
-        case 'windows':
-            commands = OsCommands.windows
-            break;
-        case 'mac':
-        default:
-            commands = OsCommands.mac
-            break;
+      case 'windows':
+        commands = OsCommands.windows;
+        break;
+      case 'mac':
+      default:
+        commands = OsCommands.mac;
+        break;
     }
 
-    await commands.updateOn(this.hostname, this.username, this.sshKey)
-    next(null, commands.isOn(this.hostname))
-}
+    await commands.updateOn(this.hostname, this.username, this.sshKey);
+    next(null, commands.isOn(this.hostname));
+  }
 
-ComputerScreen.prototype.setSwitchOnCharacteristic = function (on, next) {
-    const me = this
+  setSwitchOnCharacteristic(on, next) {
+    const me = this;
     let commands;
     switch (this.osType) {
-        case 'windows':
-            commands = OsCommands.windows
-            break;
-        case 'mac':
-        default:
-            commands = OsCommands.mac
-            break;
+      case 'windows':
+        commands = OsCommands.windows;
+        break;
+      case 'mac':
+      default:
+        commands = OsCommands.mac;
+        break;
     }
 
     if (commands.isOn(this.hostname)) {
-        me.log(`Set display off`)
-        commands.turnOff(this.hostname, this.username, this.sshKey)
+      me.log('Set display off');
+      commands.turnOff(this.hostname, this.username, this.sshKey);
     } else {
-        me.log(`Set display on`)
-        commands.turnOn(this.hostname, this.username, this.sshKey)
+      me.log('Set display on');
+      commands.turnOn(this.hostname, this.username, this.sshKey);
     }
-    return next()
+    return next();
+  }
 }
 
-
+module.exports = (homebridge) => {
+  [Service, Characteristic] = [homebridge.hap.Service, homebridge.hap.Characteristic];
+  homebridge.registerAccessory('homebridge-screen-standby', 'ComputerScreen', ComputerScreen);
+};
